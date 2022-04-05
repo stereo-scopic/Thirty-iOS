7ea45ee71de3b1efd48ced09b83465e83131b4bc
hakyung <>
2022-04-05 23:45:39 +0900

    Explore 탭 일부 Rx 적용

---
+++ Thirty/Explore/ExploreModel.swift
@@ -0,0 +1,14 @@
+//
+//  ExploreModel.swift
+//  Thirty
+//
+//  Created by 송하경 on 2022/04/05.
+//
+
+import Foundation
+
+struct Explore {
+    var title: String
+    var kor_title: String
+    var image: URL
+}
--- Thirty/My/MyShareSettingVC.swift
+++ Thirty/My/MyShareSettingVC.swift
@@ -9,24 +9,19 @@ import UIKit
 
 class MyShareSettingVC: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        updateUI()
+    }
+    
+    func updateUI() {
+        
     }
     
     @IBAction func backButtonTouchUpInside(_ sender: Any) {
         self.popVC(animated: false, completion: nil)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
