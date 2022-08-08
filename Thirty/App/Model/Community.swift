fd9a3696cf7246a61094e5cd58dd91d0ee999570
hakyung9712 <hakyungsong9712@gmail.com>
2022-08-08 22:44:38 +0900

    🎨 커뮤니티 목록 구성 및 더보기 버튼 구현

---
+++ Thirty/App/Model/Community.swift
@@ -0,0 +1,20 @@
+//
+//  Community.swift
+//  Thirty
+//
+//  Created by 송하경 on 2022/07/24.
+//
+
+import Foundation
+import UIKit
+
+struct CommunityChallenge {
+    var userNickname: String
+    var challengeTitle: String
+    var challengeOrder: Int
+    var challengeName: String
+    var challengeDetail: String
+    var challengeDate: String
+    var challengeImage: UIImage?
+    var isExpanded: Bool
+}
--- Thirty/App/UI/Community/CommunityAllVC.swift
+++ Thirty/App/UI/Community/CommunityAllVC.swift
@@ -6,23 +6,42 @@
 //
 
 import UIKit
+import RxSwift
 
 class CommunityAllVC: UIViewController {
-
+    @IBOutlet weak var communityEveryOneTableView: UITableView!
+    
+    let viewModel = CommunityListViewModel()
+    let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        viewModel.everyoneChallengeObservable
+            .bind(to: communityEveryOneTableView.rx.items(cellIdentifier: CommunityListCell.identifier, cellType: CommunityListCell.self)) { _, item, cell in
+                
+                cell.nicknameLabel.text = item.userNickname
+                cell.challengeTitleLabel.text = item.challengeTitle
+                cell.challengeOrderLabel.text = "#\(item.challengeOrder + 1)"
+                cell.challengeNameLabel.text = item.challengeName
+                cell.detailLabel.text = item.challengeDetail
+                cell.challengeCreatedAtLabel.text = item.challengeDate
+//                cell.detailLabel.numberOfLines = 1
+                
+                if let image = item.challengeImage {
+                    cell.challengeImage.isHidden = false
+                    cell.challengeImage.image = image
+                } else {
+                    cell.challengeImage.isHidden = true
+                }
+                
+                cell.makeExpand = { [weak self] _ in
+                    cell.detailLabel.numberOfLines = 0
+                    self?.communityEveryOneTableView.reloadData()
+                }
+                
+            }
+            .disposed(by: disposeBag)
 
-        // Do any additional setup after loading the view.
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
