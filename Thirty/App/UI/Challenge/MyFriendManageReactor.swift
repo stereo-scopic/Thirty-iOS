c3118667ab8c3c41166f0b08fd18af61540e5d5a
hakyung <hakyung_song1@kolon.com>
2022-11-22 09:46:38 +0900

    ✨친구 관리 화면 추가(작업중)

---
+++ Thirty/App/UI/Challenge/MyFriendManageReactor.swift
@@ -0,0 +1,75 @@
+//
+//  MyFriendManageReactor.swift
+//  Thirty
+//
+//  Created by hakyung on 2022/11/22.
+//
+
+import ReactorKit
+import Moya
+
+class MyFriendManageReactor: Reactor {
+    var initialState: State = State()
+    
+    enum Action {
+        case friendButtonTapped
+        case blockButtonTapped
+    }
+    
+    enum Mutation {
+        
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .friendButtonTapped:
+            return getFriendListRx()
+        case .blockButtonTapped:
+            return getBlockUserListRx()
+        }
+    }
+    
+    private func getFriendListRx() -> Observable<Mutation> {
+        let response = Observable<Mutation>.create { observer in
+            let provider = MoyaProvider<AuthAPI>()
+            provider.request(.getFriendList) { result in
+                switch result {
+                case .success(let response):
+                    let str = String(decoding: response.data, as: UTF8.self)
+                    print(str)
+                    
+                    let result = try? response.map([Friend].self)
+                    observer.onNext(Mutation.getFriendList(result ?? []))
+                    observer.onCompleted()
+                case .failure(let error):
+                    observer.onError(error)
+                }
+            }
+            return Disposables.create()
+        }
+        return response
+    }
+    
+    private func getBlockUserListRx() -> Observable<Mutation> {
+        let response = Observable<Mutation>.create { observer in
+            let provider = MoyaProvider<AuthAPI>()
+            provider.request(.getFriendList) { result in
+                switch result {
+                case .success(let response):
+                    let str = String(decoding: response.data, as: UTF8.self)
+                    print(str)
+                    
+                    let result = try? response.map([Friend].self)
+                    observer.onNext(Mutation.getFriendList(result ?? []))
+                    observer.onCompleted()
+                case .failure(let error):
+                    observer.onError(error)
+                }
+            }
+            return Disposables.create()
+        }
+        return response
+    }
+    
+    
+}
---
+++ Thirty/App/UI/My/MyFriendManageVC.swift
@@ -0,0 +1,23 @@
+//
+//  MyFriendManageVC.swift
+//  Thirty
+//
+//  Created by hakyung on 2022/11/22.
+//
+
+import UIKit
+
+class MyFriendManageVC: UIViewController {
+
+    @IBOutlet weak var backButton: UIButton!
+    
+    @IBOutlet weak var friendListButton: UIButton!
+    @IBOutlet weak var blockListButton: UIButton!
+    @IBOutlet weak var friendTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
