65dde1900e72b0d34b17d09ddbc2a569a7faeb6d
hakyung <hakyung_song1@kolon.com>
2022-12-20 08:56:17 +0900

    🎨Explore 탭 개선 작업 진행중

--- Thirty/App/Model/Category.swift
+++ Thirty/App/Model/Category.swift
@@ -12,3 +12,13 @@ struct Category: Codable {
     var name: String?
     var description: String?
 }
+
+enum CategoryType: String {
+    case hobby = "취미"
+    case fan = "덕질"
+    case love = "연애"
+    case selfcare = "셀프케어"
+    case diet = "다이어트"
+    case fitness = "피트니스"
+    case study = "공부"
+}
