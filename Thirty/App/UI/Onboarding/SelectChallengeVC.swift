0a9ac61cb8f37b43239aaf28ceacf1dcdfb572f5
hakyung9712 <hakyungsong9712@gmail.com>
2023-01-29 17:09:25 +0900

    🎨온보딩 2번째 화면 UI 변경 / response에 따른 화면 이동처리

--- Thirty/App/UI/Onboarding/SelectChallengeVC.swift
+++ Thirty/App/UI/Onboarding/SelectChallengeVC.swift
@@ -12,7 +12,7 @@ import ReactorKit
 
 class SelectChallengeVC: UIViewController, StoryboardView {
     typealias Reactor = SelectChallengeReactor
-    var selectedItem = BehaviorRelay<String>(value: "")
+    var selectedCategory: Category?
     var disposeBag = DisposeBag()
     var selectedChallenge: Challenge?
     var challgeList: [Challenge]?
@@ -31,7 +31,7 @@ class SelectChallengeVC: UIViewController, StoryboardView {
     }
     
     override func viewWillAppear(_ animated: Bool) {
-        reactor?.action.onNext(.setChallengeByTheme(selectedItem.value))
+        reactor?.action.onNext(.setChallengeByTheme(selectedCategory?.name ?? ""))
     }
     
     func bind(reactor: SelectChallengeReactor) {
@@ -58,12 +58,22 @@ class SelectChallengeVC: UIViewController, StoryboardView {
             .subscribe(onNext: { challengeList in
                 self.challgeList = challengeList
             }).disposed(by: disposeBag)
+        
+        reactor.state
+            .map { $0.addChallengeResponse }
+            .subscribe(onNext: { responseSucceed in
+                if responseSucceed {
+                    self.performSegue(withIdentifier: "goMain", sender: self)
+                    let viewControllers = self.navigationController?.viewControllers
+                    self.navigationController?.popToViewController((viewControllers?[viewControllers!.count - 3])!, animated: true)
+                    UserDefaults.standard.setValue(true, forKey: "launched")
+                }
+            }).disposed(by: disposeBag)
     }
     
     func bindAction(_ reactor: SelectChallengeReactor) {
         collectionView.rx.modelSelected(Challenge.self)
             .subscribe(onNext: { challenge in
-                print(challenge)
                 self.selectedChallenge = challenge
             })
             .disposed(by: disposeBag)
@@ -72,24 +82,16 @@ class SelectChallengeVC: UIViewController, StoryboardView {
             .subscribe(onNext: { [weak self] indexPath in
                 self?.collectionView.visibleCells.forEach {
                     let onboardingCell = $0 as! OnboardingCollectionViewCell
-                    onboardingCell.colorView.backgroundColor = .gray600
+                    onboardingCell.contentView.backgroundColor = .gray800
                 }
                 
                 let cell = self?.collectionView.cellForItem(at: indexPath) as! OnboardingCollectionViewCell
-                cell.colorView?.backgroundColor = .white
-                
+                cell.contentView.backgroundColor = .white
             }).disposed(by: disposeBag)
         
         nextButton.rx.tap
             .subscribe(onNext: {
                 reactor.action.onNext(.selectChallenge(self.selectedChallenge?.id ?? 0))
-                // response success오면 넘어가도록 처리해야함
-                DispatchQueue.main.asyncAfter(deadline: .now() + 0.7) {
-                    self.performSegue(withIdentifier: "goMain", sender: self)
-                    let viewControllers = self.navigationController?.viewControllers
-                    self.navigationController?.popToViewController((viewControllers?[viewControllers!.count - 3])!, animated: true)
-                    UserDefaults.standard.setValue(true, forKey: "launched")
-                }
             })
             .disposed(by: disposeBag)
     }
@@ -97,7 +99,7 @@ class SelectChallengeVC: UIViewController, StoryboardView {
 
 extension SelectChallengeVC {
     @IBAction func backButtonTouchUpInside(_ sender: UIButton) {
-        self.navigationController?.popViewController(animated: false)
+        self.navigationController?.popViewController(animated: true)
     }
 }
 
